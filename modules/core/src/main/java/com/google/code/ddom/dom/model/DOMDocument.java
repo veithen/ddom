613d8b9a94b4fa9224976c97bbca6f58f10f146c
Andreas Veithen <andreas.veithen@gmail.com>
2009-10-02 21:39:33 +0000

    Added file missing in r38.

---
+++ modules/core/src/main/java/com/google/code/ddom/dom/model/DOMDocument.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2009 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.code.ddom.dom.model;
+
+import org.w3c.dom.Document;
+
+import com.google.code.ddom.DeferredParsingException;
+import com.google.code.ddom.dom.impl.NodeFactory;
+
+public interface DOMDocument extends Document, BuilderTarget {
+    NodeFactory getNodeFactory();
+    void next() throws DeferredParsingException;
+    int getStructureVersion();
+}
