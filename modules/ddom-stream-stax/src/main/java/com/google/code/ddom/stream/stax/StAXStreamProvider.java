ec6dc41ee60d4b19a1e15856f24db08f5a44c0c9
Andreas Veithen <andreas.veithen@gmail.com>
2010-01-01 20:29:13 +0000

    Moved support for InputStream, Reader and InputSource objects from generic StAX provider to Woodstox provider (and use Woodstox specific APIs).

--- modules/ddom-stream-stax/src/main/java/com/google/code/ddom/stream/stax/StAXStreamProvider.java
+++ modules/ddom-stream-stax/src/main/java/com/google/code/ddom/stream/stax/StAXStreamProvider.java
@@ -15,21 +15,9 @@
  */
 package com.google.code.ddom.stream.stax;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Map;
-
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import org.xml.sax.InputSource;
-
 import com.google.code.ddom.CommentPolicy;
-import com.google.code.ddom.NamespaceAwareness;
 import com.google.code.ddom.OptionsProcessor;
 import com.google.code.ddom.spi.Provider;
 import com.google.code.ddom.stream.spi.Consumer;
@@ -40,50 +28,8 @@ import com.google.code.ddom.stream.spi.StreamProvider;
 @Provider(name="stax")
 public class StAXStreamProvider implements StreamProvider {
     public Producer getProducer(Object source, OptionsProcessor options, boolean preserve) throws StreamException {
-        XMLStreamReader reader;
-        try {
-            // TODO: most of the source types should be handled by a StreamProvider based on Woodstox's proprietary API
-            // TODO: who actually closes the streams???
-            if (source instanceof XMLStreamReader) {
-                // TODO: we shouldn't allow this because we don't have control over the properties of the XMLStreamReader
-                reader = (XMLStreamReader)source;
-            } else if (source instanceof InputSource) {
-                XMLInputFactory factory = getFactory(options);
-                InputSource is = (InputSource)source;
-                if (is.getByteStream() != null) {
-                    // TODO: recover system ID
-                    reader = factory.createXMLStreamReader(is.getByteStream(), is.getEncoding());
-                } else if (is.getCharacterStream() != null) {
-                    // TODO: recover system ID
-                    reader = factory.createXMLStreamReader(is.getCharacterStream());
-                } else {
-                    String systemId = is.getSystemId();
-                    URL url;
-                    try {
-                        url = new URL(systemId);
-                    } catch (MalformedURLException ex) {
-                        throw new StreamException(ex);
-                    }
-                    InputStream in;
-                    try {
-                        in = url.openStream();
-                    } catch (IOException ex) {
-                        throw new StreamException(ex);
-                    }
-                    reader = factory.createXMLStreamReader(systemId, in);
-                }
-            } else if (source instanceof InputStream) {
-                reader = getFactory(options).createXMLStreamReader((InputStream)source);
-            } else if (source instanceof Reader) {
-                reader = getFactory(options).createXMLStreamReader((Reader)source);
-            } else {
-                // TODO: support for File, URL, StreamSource, DataSource (with charset encoding detection), DataHandler, etc.
-                reader = null;
-            }
-        } catch (XMLStreamException ex) {
-            throw new StreamException(ex);
-        }
-        if (reader != null) {
+        if (source instanceof XMLStreamReader) {
+            XMLStreamReader reader = (XMLStreamReader)source;
             if (options.getAndMarkAsProcessed(CommentPolicy.class) == CommentPolicy.REMOVE) {
                 reader = new CommentFilterStreamReader(reader);
             }
@@ -91,13 +37,6 @@ public class StAXStreamProvider implements StreamProvider {
         } else {
             return null;
         }
-    }
-    
-    private XMLInputFactory getFactory(OptionsProcessor options) {
-        XMLInputFactory factory = XMLInputFactory.newInstance();
-        factory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE,
-                options.getAndMarkAsProcessed(NamespaceAwareness.class) != NamespaceAwareness.DISABLE);
-        return factory;
     }
     
     public Consumer getConsumer(Object destination, OptionsProcessor options) throws StreamException {
--- modules/ddom-stream-wstx/src/main/java/com/google/code/ddom/stream/wstx/WoodstoxStreamProvider.java
+++ modules/ddom-stream-wstx/src/main/java/com/google/code/ddom/stream/wstx/WoodstoxStreamProvider.java
@@ -15,6 +15,100 @@
  */
 package com.google.code.ddom.stream.wstx;
 
-public class WoodstoxStreamProvider {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
 
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.xml.sax.InputSource;
+
+import com.ctc.wstx.api.ReaderConfig;
+import com.ctc.wstx.io.InputBootstrapper;
+import com.ctc.wstx.io.ReaderBootstrapper;
+import com.ctc.wstx.io.StreamBootstrapper;
+import com.ctc.wstx.stax.WstxInputFactory;
+import com.ctc.wstx.util.URLUtil;
+import com.google.code.ddom.CommentPolicy;
+import com.google.code.ddom.NamespaceAwareness;
+import com.google.code.ddom.OptionsProcessor;
+import com.google.code.ddom.spi.Provider;
+import com.google.code.ddom.stream.spi.Consumer;
+import com.google.code.ddom.stream.spi.Producer;
+import com.google.code.ddom.stream.spi.StreamException;
+import com.google.code.ddom.stream.spi.StreamProvider;
+import com.google.code.ddom.stream.stax.CommentFilterStreamReader;
+import com.google.code.ddom.stream.stax.StAXParser;
+
+@Provider(name="woodstox")
+public class WoodstoxStreamProvider implements StreamProvider {
+    public Producer getProducer(Object source, OptionsProcessor options, boolean preserve) throws StreamException {
+        // TODO: who actually closes the streams???
+        InputStream byteStream = null;
+        Reader characterStream = null;
+        String systemId = null;
+        String encoding = null;
+        String publicId = null;
+        if (source instanceof InputStream) {
+            byteStream = (InputStream)source;
+        } else if (source instanceof Reader) {
+            characterStream = (Reader)source;
+        } else if (source instanceof InputSource) {
+            InputSource is = (InputSource)source;
+            byteStream = is.getByteStream();
+            characterStream = is.getCharacterStream();
+            systemId = is.getSystemId();
+            encoding = is.getEncoding();
+            publicId = is.getPublicId();
+            if (byteStream == null && characterStream == null) {
+                if (systemId == null) {
+                    throw new StreamException("Invalid InputSource object");
+                } else {
+                    try {
+                        byteStream = URLUtil.inputStreamFromURL(URLUtil.urlFromSystemId(systemId));
+                    } catch (IOException ex) {
+                        throw new StreamException("Can't open stream for system ID " + systemId, ex);
+                    }
+                }
+            }
+        }
+        // TODO: support for File, URL, StreamSource, DataSource (with charset encoding detection), DataHandler, etc.
+        if (byteStream == null && characterStream == null) {
+            return null;
+        } else {
+            InputBootstrapper bs;
+            if (byteStream != null) {
+                // TODO: where do we pass the encoding?!?
+                bs = StreamBootstrapper.getInstance(publicId, systemId, byteStream);
+            } else {
+                bs = ReaderBootstrapper.getInstance(publicId, systemId, characterStream, encoding);
+            }
+            WstxInputFactory factory = new WstxInputFactory();
+            ReaderConfig config = factory.createPrivateConfig();
+            // TODO: allow to set the base configuration through configureForXXX
+            config.doSupportNamespaces(options.getAndMarkAsProcessed(NamespaceAwareness.class) != NamespaceAwareness.DISABLE);
+            XMLStreamReader reader;
+            try {
+                // The method actually returns a BasicStreamReader if that matters
+                reader = factory.createSR(config, systemId, bs, false, false);
+            } catch (XMLStreamException ex) {
+                throw new StreamException(ex);
+            }
+            if (options.getAndMarkAsProcessed(CommentPolicy.class) == CommentPolicy.REMOVE) {
+                reader = new CommentFilterStreamReader(reader);
+            }
+            return new StAXParser(reader);
+        }
+    }
+    
+    public Consumer getConsumer(Object destination, OptionsProcessor options) throws StreamException {
+        // TODO
+        return null;
+    }
+
+    public <T> T getSerializer(Class<T> serializerType, Consumer consumer, OptionsProcessor options) {
+        // TODO
+        return null;
+    }
 }
