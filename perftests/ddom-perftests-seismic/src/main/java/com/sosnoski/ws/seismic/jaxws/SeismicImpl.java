b20d0951af935922f78998a77d880f85d9207f97
Andreas Veithen <andreas.veithen@gmail.com>
2010-01-22 21:38:25 +0000

    Seismis service: preserve original WSDL.

--- perftests/ddom-perftests-seismic/src/main/java/com/sosnoski/ws/seismic/jaxws/SeismicImpl.java
+++ perftests/ddom-perftests-seismic/src/main/java/com/sosnoski/ws/seismic/jaxws/SeismicImpl.java
@@ -1,9 +1,16 @@
 package com.sosnoski.ws.seismic.jaxws;
 
+import javax.jws.WebService;
+
 import com.sosnoski.ws.seismic.types.MatchQuakes;
 import com.sosnoski.ws.seismic.types.Results;
 import com.sosnoski.ws.seismic.wsdl.SeismicInterface;
 
+@WebService(endpointInterface="com.sosnoski.ws.seismic.wsdl.SeismicInterface",
+            serviceName="Seismic",
+            portName="SeismicPort",
+            targetNamespace = "http://ws.sosnoski.com/seismic/wsdl",
+            wsdlLocation = "META-INF/wsdl/SeismicService.wsdl")
 public class SeismicImpl implements SeismicInterface {
     public Results matchQuakes(MatchQuakes parameters) {
         return QuakeBase.process(parameters);
