c6bfedc6f0fb545725b682169099e0eced29d330
Andreas Veithen <andreas.veithen@gmail.com>
2010-02-11 23:04:39 +0000

    First working version of the maven-ddom-weaver-plugin (still missing: feature to relocate the woven classes to a different package).

---
+++ modules/maven-ddom-weaver-plugin/src/main/java/com/google/code/ddom/maven/weaver/ClassWriter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2009 Andreas Veithen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.code.ddom.maven.weaver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.google.code.ddom.weaver.ClassDefinitionProcessor;
+import com.google.code.ddom.weaver.ClassDefinitionProcessorException;
+
+public class ClassWriter implements ClassDefinitionProcessor {
+    private final File outputDirectory;
+
+    public ClassWriter(File outputDirectory) {
+        this.outputDirectory = outputDirectory;
+    }
+
+    public void processClassDefinition(String name, byte[] definition) throws ClassDefinitionProcessorException {
+        File path = outputDirectory;
+        int currentComponent = 0;
+        int nextDot;
+        while ((nextDot = name.indexOf('.', currentComponent)) != -1) {
+            path = new File(path, name.substring(currentComponent, nextDot));
+            if (!path.exists() && !path.mkdir()) {
+                throw new ClassDefinitionProcessorException("Unable to create directory " + path);
+            }
+            currentComponent = nextDot+1;
+        }
+        path = new File(path, name.substring(currentComponent) + ".class");
+        try {
+            OutputStream out = new FileOutputStream(path);
+            try {
+                out.write(definition);
+            } finally {
+                out.close();
+            }
+        } catch (IOException ex) {
+            throw new ClassDefinitionProcessorException("Unable to write class " + name + " (file " + path + ")", ex);
+        }
+    }
+}
--- modules/maven-ddom-weaver-plugin/src/main/java/com/google/code/ddom/maven/weaver/WeaverMojo.java
+++ modules/maven-ddom-weaver-plugin/src/main/java/com/google/code/ddom/maven/weaver/WeaverMojo.java
@@ -15,13 +15,23 @@
  */
 package com.google.code.ddom.maven.weaver;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 import com.google.code.ddom.spi.ProviderFinder;
 import com.google.code.ddom.spi.model.Backend;
+import com.google.code.ddom.spi.model.Frontend;
 import com.google.code.ddom.weaver.ModelWeaver;
+import com.google.code.ddom.weaver.ModelWeaverException;
 
 /**
  * @goal weave
@@ -32,7 +42,8 @@ public class WeaverMojo extends AbstractMojo {
      * The backend to use. Note that the corresponding JARs must be added to the project
      * dependencies.
      * 
-     * @parameter expression="${backend}"
+     * @parameter expression="linkedlist"
+     * @required
      */
     private String backend;
     
@@ -40,19 +51,65 @@ public class WeaverMojo extends AbstractMojo {
      * The list of frontends to use. Note that the corresponding JARs must be added to the project
      * dependencies.
      * 
-     * @parameter expression="${frontends}"
+     * @parameter
      * @required
      */
     private String[] frontends;
 
+    /**
+     * @parameter expression="${project.compileClasspathElements}"
+     * @required
+     * @readonly
+     */
+    private List<String> classpathElements;
+    
+    /**
+     * @parameter expression="${project.build.outputDirectory}"
+     * @required
+     * @readonly
+     */
+    private File outputDirectory;
+    
+    private ClassLoader createClassLoader(ClassLoader parent, List<String> classpathElements) throws IOException {
+        URL urls[] = new URL[classpathElements.size()];
+        for (int i = 0; i < classpathElements.size(); i++) {
+            urls[i] = new File(classpathElements.get(i)).toURL();
+        }
+        return new URLClassLoader(urls, parent);
+    }
+    
     public void execute() throws MojoExecutionException, MojoFailureException {
+        ClassLoader cl;
+        try {
+            cl = createClassLoader(getClass().getClassLoader(), classpathElements);
+        } catch (IOException ex) {
+            throw new MojoExecutionException("Failed to create class loader", ex);
+        }
         
-        // http://maven.apache.org/guides/mini/guide-maven-classloading.html
-        // http://old.nabble.com/Problem-with-classloader-in-maven-plugin-td17339940.html
+        Backend backendImpl = ProviderFinder.find(cl, Backend.class).get(backend);
+        if (backendImpl == null) {
+            throw new MojoFailureException("Backend '" + backend + "' not found");
+        }
         
-        // ProviderFinder.find(classLoader, Backend.class);
+        Map<String,Frontend> frontendMap = ProviderFinder.find(cl, Frontend.class);
+        Map<String,Frontend> frontendImpls = new LinkedHashMap<String,Frontend>();
+        for (String frontend : frontends) {
+            Frontend frontendImpl = frontendMap.get(frontend);
+            if (frontendImpl == null) {
+                throw new MojoFailureException("Frontend '" + frontend + "' not found");
+            }
+            frontendImpls.put(frontend, frontendImpl);
+        }
         
-        // new ModelWeaver(classLoader, processor, backend);
+        if (!outputDirectory.mkdirs()) {
+            throw new MojoFailureException("Unable to create directory " + outputDirectory);
+        }
         
+        try {
+            ModelWeaver weaver = new ModelWeaver(cl, new ClassWriter(outputDirectory), backendImpl);
+            weaver.weave(frontendImpls);
+        } catch (ModelWeaverException ex) {
+            throw new MojoExecutionException("Weaving failed", ex);
+        }
     }
 }
