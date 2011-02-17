b5961b2b9dd83ba7135b226b8846a1c728b77c93
Andreas Veithen <andreas.veithen@gmail.com>
2011-02-17 16:24:32 +0000

    Removed unused imports.

--- modules/ddom-saaj-impl/src/main/java/com/googlecode/ddom/saaj/SOAPPartImpl.java
+++ modules/ddom-saaj-impl/src/main/java/com/googlecode/ddom/saaj/SOAPPartImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 Andreas Veithen
+ * Copyright 2009-2011 Andreas Veithen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,32 +16,10 @@
 package com.googlecode.ddom.saaj;
 
 import java.io.InputStream;
-import java.util.Iterator;
 
-import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPPart;
 import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
-
-import org.w3c.dom.Attr;
-import org.w3c.dom.CDATASection;
-import org.w3c.dom.Comment;
-import org.w3c.dom.DOMConfiguration;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
-import org.w3c.dom.DocumentFragment;
-import org.w3c.dom.DocumentType;
-import org.w3c.dom.Element;
-import org.w3c.dom.EntityReference;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.ProcessingInstruction;
-import org.w3c.dom.Text;
-import org.w3c.dom.UserDataHandler;
 
 import com.google.code.ddom.DocumentHelper;
 import com.google.code.ddom.DocumentHelperFactory;
