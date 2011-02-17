87abc39bd32a2618405aa77cc3091003bd7ff188
Andreas Veithen <andreas.veithen@gmail.com>
2011-02-17 16:16:23 +0000

    Removed a dependency on DocumentHelper.

--- modules/ddom-frontend-dom/src/test/java/com/google/code/ddom/frontend/dom/DDOMUtil.java
+++ modules/ddom-frontend-dom/src/test/java/com/google/code/ddom/frontend/dom/DDOMUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 Andreas Veithen
+ * Copyright 2009-2011 Andreas Veithen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,41 +19,54 @@ import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 
-import com.google.code.ddom.DocumentHelper;
-import com.google.code.ddom.DocumentHelperFactory;
 import com.google.code.ddom.Options;
+import com.google.code.ddom.frontend.dom.intf.DOMDocument;
 import com.google.code.ddom.model.ModelDefinition;
 import com.google.code.ddom.model.ModelDefinitionBuilder;
+import com.googlecode.ddom.model.Model;
+import com.googlecode.ddom.model.ModelRegistry;
+import com.googlecode.ddom.model.spi.ModelLoaderException;
+import com.googlecode.ddom.stream.StreamException;
+import com.googlecode.ddom.stream.StreamFactory;
 import com.googlecode.ddom.stream.options.NamespaceAwareness;
 
 public class DDOMUtil extends DOMUtil {
     public static final DDOMUtil INSTANCE = new DDOMUtil();
     
-    private final ModelDefinition modelDefinition;
-    private final DocumentHelper documentHelper;
+    private final Model model;
+    private final StreamFactory streamFactory;
     
     private DDOMUtil() {
-        ModelDefinitionBuilder modelDefinitionBuilder = new ModelDefinitionBuilder();
-        modelDefinitionBuilder.addFrontend("dom");
-        modelDefinition = modelDefinitionBuilder.buildModelDefinition();
-        documentHelper = DocumentHelperFactory.INSTANCE.newInstance();
+        ModelRegistry modelRegistry = ModelRegistry.getInstance(DDOMUtil.class.getClassLoader());
+        try {
+            model = modelRegistry.getModel(ModelDefinitionBuilder.buildModelDefinition("dom"));
+        } catch (ModelLoaderException ex) {
+            throw new Error(ex);
+        }
+        streamFactory = StreamFactory.getInstance(DDOMUtil.class.getClassLoader());
     }
     
     @Override
     public Document newDocument() {
-        return (Document)documentHelper.newDocument(modelDefinition);
+        return (Document)model.getNodeFactory().createDocument();
     }
 
     @Override
     public Document parse(boolean namespaceAware, InputSource source) {
+        DOMDocument document = (DOMDocument)model.getNodeFactory().createDocument();
         // TODO: need to cleanup somehow
         Options options = new Options();
         options.set(NamespaceAwareness.get(namespaceAware));
-        return (Document)documentHelper.parse(modelDefinition, source, options);
+        try {
+            document.coreSetContent(streamFactory.getSource(source, options, false));
+        } catch (StreamException ex) {
+            throw new Error(ex);
+        }
+        return document;
     }
 
     @Override
     public DOMImplementation getDOMImplementation() {
-        return documentHelper.getAPIObject(modelDefinition, DOMImplementation.class);
+        return (DOMImplementation)model.getAPIObjectFactory().getAPIObject(DOMImplementation.class);
     }
 }
