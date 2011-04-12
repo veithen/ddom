7253fdf7a3829987ddbfc4c5276b36d371b3fe49
Andreas Veithen <andreas.veithen@gmail.com>
2011-04-12 22:03:10 +0000

    More parser code.

--- modules/ddom-core/src/main/java/com/googlecode/ddom/stream/parser/Parser.java
+++ modules/ddom-core/src/main/java/com/googlecode/ddom/stream/parser/Parser.java
@@ -32,6 +32,7 @@ public class Parser {
     private final UnicodeReader reader;
     
     private int state;
+    private int nextChar = -2;
     private char[] nameBuffer = new char[32];
     private int nameLength;
     
@@ -39,6 +40,17 @@ public class Parser {
         this.reader = reader;
     }
 
+    private int peek() throws IOException {
+        if (nextChar == -2) {
+            nextChar = reader.read();
+        }
+        return nextChar;
+    }
+    
+    private void consume() {
+        nextChar = -2;
+    }
+    
     public void parse() throws IOException {
         outer: while (true) {
             switch (state) {
@@ -75,17 +87,139 @@ public class Parser {
         }
     }
     
+    private void parseMarkup() {
+        
+    }
+    
     private void appendNameChar(int c) {
         
     }
     
+    /**
+     * Check whether the given character matches the <tt>NameStartChar</tt> production in the XML
+     * spec. Note that in the fifth edition of XML 1.0, this production has been drastically
+     * simplified, so that it is no longer necessary to use a lookup table (as e.g. Xerces does) to
+     * do this check efficiently.
+     * 
+     * @param c
+     *            the character to check
+     * @return <code>true</code> if the character matches the <tt>NameStartChar</tt> production,
+     *         <code>false</code> otherwise
+     */
     private static boolean isNameStartChar(int c) {
-        // TODO
-        return true;
+        // From XML 1.0 fifth edition:
+        //
+        // NameStartChar ::= ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF] |
+        //                   [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] |
+        //                   [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] |
+        //                   [#x10000-#xEFFFF]
+        //
+        if (c < 'A') {
+            return c == ':';
+        } else if (c <= 'Z') {
+            return true;
+        } else if (c < 'a') {
+            return c == '_';
+        } else if (c <= 'z') {
+            return true;
+        } else if (c < 0xC0) {
+            return false;
+        } else if (c <= 0x2FF) {
+            return c != 0xD7 && c != 0xF7;
+        } else if (c < 0x370) {
+            return false;
+        } else if (c <= 0x1FFFF) {
+            return c != 0x37E;
+        } else if (c < 0x2070) {
+            return c == 0x200C || c == 0x200D;
+        } else if (c <= 0x218F) {
+            return true;
+        } else if (c < 0x2C00) {
+            return false;
+        } else if (c <=0x2FEF) {
+            return true;
+        } else if (c < 0x3001) {
+            return false;
+        } else if (c <= 0xD7FF) {
+            return true;
+        } else if (c < 0xF900) {
+            return false;
+        } else if (c <= 0xFDCF) {
+            return true;
+        } else if (c < 0xFDF0) {
+            return false;
+        } else if (c <= 0xFFFD) {
+            return true;
+        } else if (c < 0x10000) {
+            return false;
+        } else if (c <= 0xEFFFF) {
+            return true;
+        } else {
+            return false;
+        }
     }
     
+    /**
+     * Check whether the given character matches the <tt>NameChar</tt> production in the XML spec.
+     * 
+     * @param c
+     *            the character to check
+     * @return <code>true</code> if the character matches the <tt>NameChar</tt> production,
+     *         <code>false</code> otherwise
+     */
     private static boolean isNameChar(int c) {
-        // TODO
-        return true;
+        // From XML 1.0 fifth edition:
+        //
+        // NameChar ::= NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
+        //
+        // That means:
+        //
+        // NameChar ::= "-" | "." | [0-9] | ":" | [A-Z] | "_" | [a-z] | #xB7 | [#xC0-#xD6] |
+        //              [#xD8-#xF6] | [#xF8-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x203F-#x2040]
+        //              [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] |
+        //              [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
+        if (c < '0') {
+            return c == '-' || c == '.';
+        } else if (c <= ':') { // '9' and ':' are adjacent
+            return true;
+        } else if (c < 'A') {
+            return false;
+        } else if (c <= 'Z') {
+            return true;
+        } else if (c < 'a') {
+            return c == '_';
+        } else if (c <= 'z') {
+            return true;
+        } else if (c < 0xC0) {
+            return c == 0xB7;
+        } else if (c <= 0x1FFF) {
+            return c != 0xD7 && c != 0xF7 && c != 0x37E;
+        } else if (c < 0x2070) {
+            return c == 0x200C || c == 0x200D || c == 0x203F || c == 0x2040;
+        } else if (c <= 0x218F) {
+            return true;
+        } else if (c < 0x2C00) {
+            return false;
+        } else if (c <=0x2FEF) {
+            return true;
+        } else if (c < 0x3001) {
+            return false;
+        } else if (c <= 0xD7FF) {
+            return true;
+        } else if (c < 0xF900) {
+            return false;
+        } else if (c <= 0xFDCF) {
+            return true;
+        } else if (c < 0xFDF0) {
+            return false;
+        } else if (c <= 0xFFFD) {
+            return true;
+        } else if (c < 0x10000) {
+            return false;
+        } else if (c <= 0xEFFFF) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
