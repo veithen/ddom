0602678e612c2e35524ccf34711315e49104f4e6
Andreas Veithen <andreas.veithen@gmail.com>
2011-05-02 18:51:10 +0000

    Renamed package from com.google.code to com.googlecode.

--- modules/maven-ddom-weaver-plugin/src/main/java/com/googlecode/ddom/maven/weaver/ClassWriter.java
+++ modules/maven-ddom-weaver-plugin/src/main/java/com/googlecode/ddom/maven/weaver/ClassWriter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.code.ddom.maven.weaver;
+package com.googlecode.ddom.maven.weaver;
 
 import java.io.File;
 import java.io.FileOutputStream;
