0931c2d080214ab9303863b56ea675000ab3d6d6
Andreas Veithen <andreas.veithen@gmail.com>
2011-06-07 20:10:47 +0000

    Improved the weaver so that Cobertura reports are more accurate.

--- modules/ddom-weaver/src/main/java/com/googlecode/ddom/weaver/mixin/ConstructorToMethodConverter.java
+++ modules/ddom-weaver/src/main/java/com/googlecode/ddom/weaver/mixin/ConstructorToMethodConverter.java
@@ -23,16 +23,46 @@ import org.objectweb.asm.Opcodes;
 
 /**
  * Converts a constructor into a normal method. This will remove the call to the constructor of the
- * superclass. Note that this is only meaningful for default constructors.
+ * superclass. Note that this is only meaningful if the constructor is a default constructor (i.e.
+ * has no parameters) and calls the default constructor of the superclass. By definition, this is
+ * the case for mixins.
+ * <p>
+ * The class actually looks for the following sequence of instructions and removes them:
+ * 
+ * <pre>
+ *    ALOAD 0
+ *    INVOKESPECIAL java/lang/Object.&lt;init> ()V
+ * </pre>
+ * 
+ * Any instructions before and after will be preserved. This also includes the code inserted by
+ * Cobertura before the call to the constructor of the superclass. This ensures that coverage
+ * reports are accurate.
  */
 public class ConstructorToMethodConverter implements MethodVisitor {
     private final MethodVisitor mv;
-    private boolean keep;
+    
+    /**
+     * Flag indicating that the last processed instruction was ALOAD 0. This will only be set if
+     * {@link #callRemoved} is <code>false</code>.
+     */
+    private boolean lastWasALoad0;
+    
+    /**
+     * Flag indicating that the call to the constructor of the superclass has been removed.
+     */
+    private boolean callRemoved;
     
     public ConstructorToMethodConverter(MethodVisitor mv) {
         this.mv = mv;
     }
-
+    
+    private void reset() {
+        if (lastWasALoad0) {
+            mv.visitVarInsn(Opcodes.ALOAD, 0);
+            lastWasALoad0 = false;
+        }
+    }
+    
     public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
         return mv.visitAnnotation(desc, visible);
     }
@@ -58,109 +88,100 @@ public class ConstructorToMethodConverter implements MethodVisitor {
     }
 
     public void visitFieldInsn(int opcode, String owner, String name, String desc) {
-        if (keep) {
-            mv.visitFieldInsn(opcode, owner, name, desc);
-        }
+        reset();
+        mv.visitFieldInsn(opcode, owner, name, desc);
     }
 
     public void visitFrame(int type, int nLocal, Object[] local, int nStack, Object[] stack) {
-        if (keep) {
-            mv.visitFrame(type, nLocal, local, nStack, stack);
-        }
+        reset();
+        mv.visitFrame(type, nLocal, local, nStack, stack);
     }
 
     public void visitIincInsn(int var, int increment) {
-        if (keep) {
-            mv.visitIincInsn(var, increment);
-        }
+        reset();
+        mv.visitIincInsn(var, increment);
     }
 
     public void visitInsn(int opcode) {
-        if (keep) {
-            mv.visitInsn(opcode);
-        }
+        reset();
+        mv.visitInsn(opcode);
     }
 
     public void visitIntInsn(int opcode, int operand) {
-        if (keep) {
-            mv.visitIntInsn(opcode, operand);
-        }
+        reset();
+        mv.visitIntInsn(opcode, operand);
     }
 
     public void visitJumpInsn(int opcode, Label label) {
-        if (keep) {
-            mv.visitJumpInsn(opcode, label);
-        }
+        reset();
+        mv.visitJumpInsn(opcode, label);
     }
 
     public void visitLabel(Label label) {
+        reset();
         mv.visitLabel(label);
     }
 
     public void visitLdcInsn(Object cst) {
-        if (keep) {
-            mv.visitLdcInsn(cst);
-        }
+        reset();
+        mv.visitLdcInsn(cst);
     }
 
     public void visitLineNumber(int line, Label start) {
-        if (keep) {
-            mv.visitLineNumber(line, start);
-        }
+        reset();
+        mv.visitLineNumber(line, start);
     }
 
     public void visitLocalVariable(String name, String desc, String signature, Label start, Label end, int index) {
-        if (keep) {
-            mv.visitLocalVariable(name, desc, signature, start, end, index);
-        }
+        reset();
+        mv.visitLocalVariable(name, desc, signature, start, end, index);
     }
 
     public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
-        if (keep) {
-            mv.visitLookupSwitchInsn(dflt, keys, labels);
-        }
+        reset();
+        mv.visitLookupSwitchInsn(dflt, keys, labels);
     }
 
     public void visitMaxs(int maxStack, int maxLocals) {
-        if (keep) {
-            mv.visitMaxs(maxStack, maxLocals);
-        }
+        reset();
+        mv.visitMaxs(maxStack, maxLocals);
     }
 
     public void visitMethodInsn(int opcode, String owner, String name, String desc) {
-        if (keep) {
+        if (lastWasALoad0 && opcode == Opcodes.INVOKESPECIAL) {
+            lastWasALoad0 = false;
+            callRemoved = true;
+        } else {
+            reset();
             mv.visitMethodInsn(opcode, owner, name, desc);
-        } else if (opcode == Opcodes.INVOKESPECIAL) {
-            keep = true;
         }
     }
 
     public void visitMultiANewArrayInsn(String desc, int dims) {
-        if (keep) {
-            mv.visitMultiANewArrayInsn(desc, dims);
-        }
+        reset();
+        mv.visitMultiANewArrayInsn(desc, dims);
     }
 
     public void visitTableSwitchInsn(int min, int max, Label dflt, Label[] labels) {
-        if (keep) {
-            mv.visitTableSwitchInsn(min, max, dflt, labels);
-        }
+        reset();
+        mv.visitTableSwitchInsn(min, max, dflt, labels);
     }
 
     public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
-        if (keep) {
-            mv.visitTryCatchBlock(start, end, handler, type);
-        }
+        reset();
+        mv.visitTryCatchBlock(start, end, handler, type);
     }
 
     public void visitTypeInsn(int opcode, String type) {
-        if (keep) {
-            mv.visitTypeInsn(opcode, type);
-        }
+        reset();
+        mv.visitTypeInsn(opcode, type);
     }
 
     public void visitVarInsn(int opcode, int var) {
-        if (keep) {
+        reset();
+        if (!callRemoved && opcode == Opcodes.ALOAD && var == 0) {
+            lastWasALoad0 = true;
+        } else {
             mv.visitVarInsn(opcode, var);
         }
     }
